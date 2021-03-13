cde49ff33b5f7022bca89f677b5b09e523a38760
kitdea <francismarrosales@gmail.com>
2021-03-14 05:35:26 +0800

    fixed menu not showing in desktop after using it in mobile

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
 $('.ita-toggle-btn').click(function(){
     $('.ita-navmenu').slideToggle(280);
-  });
-
-
-// const menuBtn = document.getElementsByClassName('.ita-toggle-btn')[0];
-// const navMenu = document.getElementsByClassName('.ita-navmenu')[0];
-
-// menuBtn.addEventListener('click', () => {
-//     navMenu.classList.toggle('active');
-// });
+});
