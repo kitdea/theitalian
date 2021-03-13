7ff89d5b5ea268643429b407ba8e17c8ce95a4e3
kitdea <francismarrosales@gmail.com>
2021-03-14 05:04:48 +0800

    fixed mobile menu

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.ita-toggle-btn').click(function(){
+    $('.ita-navmenu').slideToggle(280);
+  });
+
+
+// const menuBtn = document.getElementsByClassName('.ita-toggle-btn')[0];
+// const navMenu = document.getElementsByClassName('.ita-navmenu')[0];
+
+// menuBtn.addEventListener('click', () => {
+//     navMenu.classList.toggle('active');
+// });
